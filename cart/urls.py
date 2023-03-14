a39d7089beea95f352af9f1c64c468303355d206
AraratAlexanyan <alexanyan.ararat1@gmail.com>
2023-03-14 16:34:44 +0400

    Initial commit

---
+++ cart/urls.py
@@ -0,0 +1,8 @@
+from django.template.defaulttags import url
+from django.urls import path
+from .views import cart, update_cart
+
+urlpatterns = [
+    path('cart/', cart, name='cart'),
+    path('update_cart/', update_cart, name='update_cart'),
+]
